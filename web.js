b88031d4b661501491f9d95f903e40f879b90d04
Ksen0s <Ksen0s@yandex.ru>
2013-07-17 08:34:10 -0700

    Update web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
   
 app.get('/', function(request, response) {
-  var buf = new Buffer(fs.readFileSync("index.html"), "utf-8");
+  var buf = fs.readFileSync("index.html");
   response.send(buf.toString);
 });
 
